06821f5962a281ed6dfeb2ed2a05fb02ebd129da
CARLOS HENRIQUE CAVALCANTE RODRIGUES LIBERALINO SILVA <143114355+Liberalino@users.noreply.github.com>
2024-06-22 02:55:50 -0400

    :tada: initial commit

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function togglemode(){
     
         //subdtituir a imagem
  if(html.classList.contains('light')){
-    img.setAttribute("src","avatar2.png") 
+    img.setAttribute("src","img/avatar2.png") 
     img.setAttribute("alt", "carlos henrique com fundo branco")
            //se tiver ligth mode, adicionar a imagem light
  }else{
     
         // se tiver sem light mode, manter imagem normal 
-img.setAttribute("src","avatar1.png")
+img.setAttribute("src","img/avatar1.png")
 img.setAttribute("alt", "carlos henrique com fundo preto")
  }
  
